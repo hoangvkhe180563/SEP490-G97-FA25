f5c6f2188b994bcc444dd97234778ca761fcf993
hoangvkhe180563 <hoangvkhe180563@fpt.edu.vn>
2025-09-29 23:04:10 +0700

    Add frontend project

---
+++ StudyHub.Frontend/src/main/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/common/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="font-bold">Hello World!</div>
+      <span className="text-blue-800">StudyHub màu xanh đậm</span>
+      <Button variant="destructive">Click me</Button>
+    </>
+  )
+}
+
+export default App
