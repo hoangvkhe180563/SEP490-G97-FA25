bdadd31dbfa4bb6cd43dedb14176853f07b1865e
NGHIADTHE186997 <nghiadthe186997@fpt.edu.vn>
2025-10-14 16:01:06 +0700

    Add CORS policy and connection string config
    Configured CORS to allow requests from React development servers and added a connection string to appsettings.json for database access.

--- StudyHub.Backend/StudyHub.Backend.Api/Program.cs
+++ StudyHub.Backend/StudyHub.Backend.Api/Program.cs
@@ -8,8 +8,18 @@ builder.Services.AddControllers();
 builder.Services.AddUseCasesDependency()
                 .AddInfrastructureDependency(builder.Configuration.GetConnectionString("value") ?? "");
 //Để chỉnh cái connection string, chuột phải project chọn Manage user secrets.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 var app = builder.Build();
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
