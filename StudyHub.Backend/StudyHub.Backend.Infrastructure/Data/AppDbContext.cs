cc81b44a928a174c8963ce0365e9c53b9101dc6e
hoangvkhe180563 <hoangvkhe180563@fpt.edu.vn>
2025-10-15 15:00:12 +0700

    fix db connect

--- StudyHub.Backend/StudyHub.Backend.Infrastructure/Data/AppDbContext.cs
+++ StudyHub.Backend/StudyHub.Backend.Infrastructure/Data/AppDbContext.cs
@@ -68,10 +68,6 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<SubmissionFile> SubmissionFiles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=studyhub;user=root;password=123456", ServerVersion.Parse("8.0.43-mysql"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
