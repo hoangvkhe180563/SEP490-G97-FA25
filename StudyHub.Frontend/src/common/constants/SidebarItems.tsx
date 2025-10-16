e54febe7420037780b93ae17203ea66dd341d062
hoangvkhe180563 <hoangvkhe180563@fpt.edu.vn>
2025-10-16 08:36:38 +0700

    Add header and sidebar

---
+++ StudyHub.Frontend/src/common/constants/SidebarItems.tsx
@@ -0,0 +1,37 @@
+import { BookOpen, FileQuestionMark, LayoutDashboard, LibraryBig, Users } from "lucide-react";
+import type { ISidebarItem } from "../interfaces/IMainLayoutProps";
+
+export const guestSidebarItems: ISidebarItem[] = [
+  {
+    icon: <LayoutDashboard size={20} />,
+    text: 'Trang chủ',
+    link: '/'
+  },
+  {
+    icon: <LibraryBig size={20} />,
+    text: 'Tài liệu',
+    link: '',
+    children: [
+      {
+        icon: <BookOpen size={20} />,
+        text: 'Sách giáo khoa',
+        link: '/document/textbooks'
+      },
+      {
+        icon: <FileQuestionMark size={20} />,
+        text: 'Sách tham khảo',
+        link: '/document/references'
+      }
+    ]
+  },
+  {
+    icon: <BookOpen size={20} />,
+    text: 'Khóa học',
+    link: '/courses'
+  },
+  {
+    icon: <Users size={20} />,
+    text: 'Lớp học của tôi',
+    link: '/classes'
+  }
+]
--- StudyHub.Frontend/src/main/AppRouter.tsx
+++ StudyHub.Frontend/src/main/AppRouter.tsx
@@ -2,20 +2,24 @@ import classRoutes from "@/classManagement/routes/ClassRoutes";
 import documentRoutes from "@/documentManagement/routes/DocumentRoutes";
 
 import RouteConfig from "@/common/constants/RouteConfig";
-import Homepage from "@/uiManagement/pages/Homepage";
 import uiManagementRoutes from "@/uiManagement/routes/UiManagementRoutes";
 import userRoutes from "@/user/routes/UserRoutes";
 import { Outlet, useRoutes } from "react-router-dom";
+import MainLayout from "@/common/pages/MainLayout";
+import Homepage from "@/uiManagement/pages/Homepage";
+import { guestSidebarItems } from "@/common/constants/SidebarItems";
 
 const AppRouter = () => {
   const appRoutes = [
     {
       path: "/",
-      element: <Homepage />
-    },
-    {
-      path: "/:school",
-      element: <Homepage />
+      element: <MainLayout isLoggedIn={false} sidebarItems={guestSidebarItems} />,
+      children: [
+        {
+          index: true,
+          element: <Homepage />
+        }
+      ]
     },
     {
       path: RouteConfig.USER,
@@ -32,7 +36,7 @@ const AppRouter = () => {
       element: <Outlet />,
       children: classRoutes
     },
-     {
+    {
       path: RouteConfig.DOCUMENT_MANAGEMENT,
       element: <Outlet />,
       children: documentRoutes
