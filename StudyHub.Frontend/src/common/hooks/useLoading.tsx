ff3d9ff175a0894eff559e6bb520d58d9b03e6ea
hoangvkhe180563 <hoangvkhe180563@fpt.edu.vn>
2025-10-22 10:43:24 +0700

    add loading hook

---
+++ StudyHub.Frontend/src/common/hooks/useLoading.tsx
@@ -0,0 +1,31 @@
+import { LoaderIcon } from "lucide-react";
+import { createContext, useContext, useState } from "react";
+
+interface LoadingType {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoadingContext = createContext<LoadingType | undefined>(undefined);
+
+export const useLoading = () => {
+  const context = useContext(LoadingContext);
+  if (context === undefined) {
+    throw new Error('useLoading must be used within a LoadingContextProvider');
+  }
+  return context;
+};
+
+export const LoadingContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const contextValue = { setLoading };
+
+  return (
+    <LoadingContext.Provider value={contextValue}>
+      {loading && <div className="absolute inset-0 bg-gray-500/75 flex justify-center items-center z-1000 select-none">
+        <LoaderIcon size={50} className="stroke-white animate-spin" />
+      </div>}
+      {children}
+    </LoadingContext.Provider>
+  );
+}
