1b0c25c356bc8c14cc9873b6f88df41ce244de5c
hoangvkhe180563 <hoangvkhe180563@fpt.edu.vn>
2025-10-25 10:15:27 +0700

    [IMPORTANT] FIX THE NEW DB!

---
+++ StudyHub.Backend/StudyHub.Backend.Infrastructure/Data/AppPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudyHub.Backend.Infrastructure.Data;
+
+public partial class AppPolicy
+{
+    public Guid RoleId { get; set; }
+
+    public int ResourceId { get; set; }
+
+    public string ActionType { get; set; } = null!;
+
+    public string? Condition { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual AppResource Resource { get; set; } = null!;
+
+    public virtual AppRole Role { get; set; } = null!;
+}
