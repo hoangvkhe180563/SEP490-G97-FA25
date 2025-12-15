9319c9d9af86012ebc777d6e5c3055bb7c9cf645
John Doe <nghianvhe176638@fpt.edu.vn>
2025-12-15 21:45:22 +0700

    Fix ui bug

--- StudyHub.Frontend/src/recommend/pages/statistics.tsx
+++ StudyHub.Frontend/src/recommend/pages/statistics.tsx
@@ -7,7 +7,7 @@ import StatsTokens from "../components/StatsTokens";
 const RecommendStatistics: React.FC = () => {
   return (
     <div className="p-6 space-y-6 max-h-screen min-h-screen overflow-auto scrollbar-x-hide">
-      <h1 className="text-2xl font-semibold">Bảng thống kê AI / LLM</h1>
+      <h1 className="text-2xl font-semibold">Bảng thống kê đề xuất</h1>
 
       <div className="space-y-6">
         <div className="w-full">
